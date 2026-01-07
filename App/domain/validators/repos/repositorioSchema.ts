097990551c5530c46554d49cac4be02c98a6a175
yukio <zviamontevilca@gmail.com>
2026-01-07 17:46:20 -0500

    submit

--- App/domain/validators/repos/repositorioSchema.ts
+++ App/domain/validators/repos/repositorioSchema.ts
@@ -5,7 +5,7 @@ import { Validator } from "../validators.js";
 export const repositorioSchema = {
 
     nombrerepo:z.string().trim().max(200),
-    descripcion:z.string().trim().min(200).refine(val => Validator.isValidDescription(val),{
+    descripcion:z.string().trim().max(200).refine(val => Validator.isValidDescription(val),{
         error:"No esta permitido caracteres especailes"
     }),
     dominio:z.string().trim().max(100).refine( val => Validator.isValidFullName(val),{
@@ -14,9 +14,7 @@ export const repositorioSchema = {
     ipdata:z.string().trim().max(100).refine(val => Validator.isValidIp(val),{
         error:" IP invalido "
     }),
-    orgdata:z.string().trim().max(20).refine(val => Validator.isValidGeneralName(val),{
-        error:"Org no puede contener caracteres especiales"
-    }),
+    orgdata:z.string().trim().max(20),
     portdata:z.string().trim().max(10).refine(val => Validator.isOnlyNumbers(val),{
         error:"El puerto solo contiene numeros "
     }),
