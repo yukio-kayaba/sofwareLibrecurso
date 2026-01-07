fee866b27ec2ea7fb4b5175ecf9649c1e8015daa
yukio <zviamontevilca@gmail.com>
2026-01-07 18:09:34 -0500

    Revert "prueba 1"
    This reverts commit b114cb1a17812eaa0093c91a403c173025bf4037.

--- App/presentation/entities/route.ts
+++ App/presentation/entities/route.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { ReposRoutes } from "./repos/route.js";
-import { TemplatesRoutes } from "../templates/templates.routes.js";
 
 export class EntitiesRoutes {
   static get routes() {
     const router = Router();
 
     router.use("/repositorios",ReposRoutes.routes);
-    router.use("/templates", TemplatesRoutes.routes);
     
 
     return router;
